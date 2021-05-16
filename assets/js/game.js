492a029c5fcfcc723aee428c4e23c9aafe519705
Andrew Bwogi <andrew.bwogi@gmail.com>
2021-05-15 20:31:07 -0700

    more addition to the feature

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,15 @@
-alert("Yey! JS is running... ;)");
+// alert("Yey! JS is running... ;)");
+let playerName = prompt("What is the name of your robot?!");
+console.log("This logs a string, good for leaving yourself a message");
+
+console.log(10 + 10);
+
+console.log(playerName);
+console.log("Our robot's name is " + playerName);
+
+
+function fight() {
+    alert("The fight has begun!");
+}
+
+// fight();
