9bcee4738f7f6c32f1f0822095b61e3cf9bb3f2b
Andrew Bwogi <andrew.bwogi@gmail.com>
2021-05-27 09:03:21 -0700

    commitments

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,9 +6,15 @@ var playerMoney = 10;
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
+
+for (var i = 0; i < enemyNames.length; i++) {
+    console.log(enemyNames[i]);
+    console.log(i);
+    console.log(enemyName[i] + " is at " + i + " index");
+}
 
 var fight = function() {
     // Alert players that they are starting the round
