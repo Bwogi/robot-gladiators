aaae135d67e57e61ec397b08fbf012e4bde31ad4
Andrew Bwogi <andrew.bwogi@gmail.com>
2023-06-05 19:43:54 -0700

    new structure

---
+++ assets/js/game.js
@@ -0,0 +1,9 @@
+
+
+function fight() {
+    let robotName = window.prompt("What is your robot's name");
+    let PlayerName = window.prompt("...and what is your name");
+    window.alert(`Hi ${PlayerName}, we welcome your robot ${robotName}, to this awesome game.`);
+}
+
+fight();
